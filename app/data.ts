7addaa2d1fa69d0678385860351ad513e66e530d
David McCaig <davidmccaig1@gmail.com>
2025-07-01 11:56:36 -0400

    Create env example file

--- app/data.ts
+++ app/data.ts
@@ -5,7 +5,7 @@ import { cookies } from "next/headers";
 
 export async function getAccountId(token: string) {
     const cookieStore = await cookies();
-
+    const lightSpeedApiUrl = process?.env?.LIGHTSPEED_API_URL;
     const storedAccountId = cookieStore.get("lightspeed_account_id")?.value;
 
     if (!token) {
@@ -14,7 +14,7 @@ export async function getAccountId(token: string) {
 
 
     // Fetch account ID if not cached
-    const response = await axios.get("https://api.lightspeedapp.com/API/V3/Account.json", {
+    const response = await axios.get(`${lightSpeedApiUrl}/API/V3/Account.json`, {
         headers: {
             "Authorization": `Bearer ${token}`
         }
@@ -34,14 +34,14 @@ export async function isTokenValid(): Promise<boolean> {
     try {
         const cookieStore = await cookies();
         const token = cookieStore.get("lightspeed_token")?.value;
-
+        const lightSpeedApiUrl = process?.env?.LIGHTSPEED_API_URL;
         if (!token) {
             return false;
         }
 
         // Make a simple API call to test if the token is still valid
         // Using the Account endpoint as it's lightweight and commonly used
-        await axios.get("https://api.lightspeedapp.com/API/V3/Account.json", {
+        await axios.get(`${lightSpeedApiUrl}/API/V3/Account.json`, {
             headers: {
                 "Authorization": `Bearer ${token}`
             }
@@ -63,11 +63,15 @@ export async function getAccountDetails() {
         throw new Error("No token found");
     }
 
-    const response = await axios.get("https://api.lightspeedapp.com/API/V3/Account.json", {
-        headers: {
-            "Authorization": `Bearer ${token}`
-        }
-    });
+    const lightSpeedApiUrl = process?.env?.LIGHTSPEED_API_URL;
+
+  const response = await axios.get(
+    `${lightSpeedApiUrl}/API/V3/Account.json`,
+    {
+      headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
 
     return response?.data;
 }
