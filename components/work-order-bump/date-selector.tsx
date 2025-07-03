47110e0118386f67948c906f10baa79b9877712e
David McCaig <davidmccaig1@gmail.com>
2025-07-03 16:09:47 -0400

    Remove function to convert date to string

--- components/work-order-bump/date-selector.tsx
+++ components/work-order-bump/date-selector.tsx
@@ -27,17 +27,6 @@ export function DateSelector({ fromDate, toDate, onFromDateChange, onToDateChang
     }
   }
 
-  function formatDateString(dateString: string | Date): string {
-    const date = new Date(dateString)
-
-    const year = date.toLocaleString('default', { year: 'numeric' })
-    const month = date.toLocaleString('default', { month: '2-digit' })
-    const day = date.toLocaleString('default', { day: '2-digit' })
-
-    // Generate yyyy-mm-dd date string
-    return year + '-' + month + '-' + day
-}
-
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
       {/* From Date Calendar */}
